1701d9cd5887a2d8bbdd3c235cf9d1f938a64218
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-03-02 03:09:06 +0900

    feat: 레코드 테이블 열 구성을 객체지향적으로 처리할 수 있도록 함

--- src/constants/getColumnByRecords.tsx
+++ src/constants/getColumnByRecords.tsx
@@ -1,35 +1,143 @@
-import { Button, Checkbox, TableProps } from 'antd';
+import { Checkbox, MenuProps, TableProps } from 'antd';
 import { Label } from '../types/Label';
 import { Record } from '../types/Record';
-import { Key } from 'react';
+import { Key, ReactElement } from 'react';
+import KebabMenu from '../components/Kebab';
+import { useRecordListStore } from '../hooks/globalState/RecordList';
+import { DialogStore } from '../components/Dialog/DialogStore';
+import Dialog from '../components/Dialog/Dialog';
+import RecordForm from '../components/Form/RecordForm';
 
 export const getColumnByRecords = <T extends Array<Record>>(
   records: T,
 ): TableProps<Record>['columns'] => {
   const columns: TableProps<Record>['columns'] = [];
 
-  for (const [key] of Object.entries(records[0])) {
-    if (key === 'key') continue;
+  // 추후 전역 상태로 분리
+  const ColumnList = [
+    '이름',
+    '주소',
+    '메모',
+    '가입일',
+    '직업',
+    '이메일 수신 동의',
+    '레코드 수정 버튼',
+  ];
 
-    columns.push({
-      title: key,
-      dataIndex: key,
-      filters: records.map((record) => ({
-        text: record[key as Label],
-        value: record[key as Label] as boolean | Key,
-      })),
-      onFilter: (value: boolean | Key, record: Record) => record['이름'].includes(value as string),
-      render: (text) =>
-        key === '이메일 수신 동의' ? <Checkbox checked={text} /> : <span>{text}</span>,
-    });
+  for (const key of ColumnList) {
+    columns.push(new ColumnFactory().createColumn(key as Label, records));
   }
-
-  columns.push({
-    title: '',
-    className: 'text-bold',
-    dataIndex: 'id',
-    render: (text: string) => <Button type="primary">{text}</Button>,
-  });
 
   return columns;
 };
+
+class ColumnFactory {
+  private columnMap: Map<string, new () => Column> = new Map([
+    ['이메일 수신 동의', EmailAgreementColumn],
+    ['레코드 수정 버튼', RecordEditKebabColumn],
+  ]);
+
+  createColumn(key: string, records: Record[]): IColumnConfig {
+    const ColumnClass = this.columnMap.get(key);
+
+    if (ColumnClass) {
+      return new ColumnClass().exec(key, records);
+    } else {
+      return new BasicColumn().exec(key, records);
+    }
+  }
+}
+
+interface IColumnConfig {
+  title: string;
+  dataIndex: string;
+  width?: number;
+  filters?: { text: string; value: string }[];
+  onFilter?: (value: boolean | Key, record: Record) => boolean;
+  render?: (value: string, record: Record, index: number) => ReactElement;
+}
+
+interface Column {
+  exec(key: string, records: Record[]): IColumnConfig;
+}
+
+class RecordEditKebabColumn implements Column {
+  private useRecordListStore = useRecordListStore();
+  private items: MenuProps['items'] = [
+    {
+      key: '수정',
+      label: '수정',
+    },
+    {
+      key: '삭제',
+      label: '삭제',
+      danger: true,
+    },
+  ];
+  private handleRecordEditMenuClick: (value: string, record: Record) => MenuProps['onClick'] =
+    (value: string, record: Record) => (e) => {
+      if (e.key === '수정') {
+        DialogStore.store = (
+          <>
+            <Dialog.Title title="레코드 추가" />
+            <RecordForm initValue={record} />
+            <Dialog.Footer disabled={false} onClick={() => {}}>
+              저장
+            </Dialog.Footer>
+          </>
+        );
+      } else if (e.key === '삭제') {
+        if (confirm('정말 삭제하시겠습니까?')) {
+          this.useRecordListStore[1]((store) => store.filter((record) => record.key !== value));
+        }
+      }
+    };
+
+  exec(): IColumnConfig {
+    return {
+      title: '',
+      dataIndex: 'key',
+      width: 48,
+      render: (value: string, record: Record) => {
+        return (
+          <>
+            <KebabMenu
+              items={this.items}
+              handleMenuClick={this.handleRecordEditMenuClick(value, record)}
+            />
+          </>
+        );
+      },
+    };
+  }
+}
+
+class EmailAgreementColumn implements Column {
+  exec(key: string, records: Record[]): IColumnConfig {
+    return {
+      title: key,
+      dataIndex: key,
+      filters: records.map((record: Record) => ({
+        text: String(record[key as keyof Record]),
+        value: String(record[key as keyof Record]),
+      })),
+      onFilter: (value: boolean | Key, record: Record) => record['이름'].includes(value as string),
+      render: (text) => <Checkbox checked={text as unknown as boolean} />,
+    };
+  }
+}
+
+class BasicColumn implements Column {
+  exec(key: string, records: Record[]): IColumnConfig {
+    return {
+      title: key,
+      dataIndex: key,
+      filters: records.map((record: Record) => ({
+        text: String(record[key as keyof Record]),
+        value: String(record[key as keyof Record]),
+      })),
+      onFilter: (value: boolean | Key, record: Record) =>
+        String(record[key as keyof Record]).includes(value as string),
+    };
+  }
+}
