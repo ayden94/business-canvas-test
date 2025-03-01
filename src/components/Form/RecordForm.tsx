6d40a1a9470bd147924fb5dd94010281e06782a8
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-03-02 03:03:55 +0900

    refactor : RecordForm을 세부적으로 분리

--- src/components/Form/RecordForm.tsx
+++ src/components/Form/RecordForm.tsx
@@ -1,91 +1,157 @@
 import { DatePicker, Input, Select, Checkbox } from 'antd';
 import { SELECT_JOB_OPTIONS } from '../../constants/select';
-import { useForm } from 'sicilian/useForm';
-import { SicilianEvent } from 'sicilian';
-import { type Label as LabelType } from '../../types/Label';
 import Label from '../Label';
 import Form from './Form';
+import { Record } from '../../types/Record';
+import dayjs from 'dayjs';
+import { useForm } from 'sicilian/useForm';
+import { useContext, useEffect } from 'react';
+import { DialogContext } from '../../hooks/Contexts/useDialogContext';
+import { SicilianProvider, useSicilianContext } from 'sicilian/provider';
 
-export default function RecordForm({ initValue }: { initValue?: Record<LabelType, unknown> }) {
-  const { register } = useForm({
-    initValue,
-  });
+export default function RecordForm({
+  initValue = {
+    key: crypto.randomUUID(),
+    이름: '',
+    주소: '',
+    가입일: '',
+    직업: SELECT_JOB_OPTIONS[0],
+  },
+}: {
+  initValue?: Record;
+}) {
+  const { register, getValues } = useForm({ initValue });
+  const { setDisabled } = useContext(DialogContext);
+  const formValues = getValues();
 
-  const selectCreateAt = register({ name: '가입일', type: 'date' });
-  const selectJob = register({ name: '직업', type: 'radio' });
-  const checkboxEmail = register({ name: '이메일 수신 동의', type: 'checkbox' });
-
-  const handlePopupContainer = (trigger: HTMLElement) => {
-    return trigger;
-  };
+  useEffect(() => {
+    if (setDisabled) {
+      setDisabled(!formValues.가입일 || !formValues.이름);
+    }
+  }, [formValues]);
 
   return (
-    <Form className="mx-24 mt-10 mb-20 flex w-520 flex-col gap-20">
-      <div>
-        <Label className="flex h-40 items-center" htmlFor="이름" required>
-          이름
-        </Label>
-        <Input {...register({ name: '이름' })} placeholder="이름" />
-      </div>
+    <SicilianProvider value={{ register, name: '이름' }}>
+      <Form className="mx-24 mt-10 mb-20 flex w-520 flex-col gap-20">
+        <RecordNameInput />
+        <RecordAddressInput />
+        <RecordMemoInput />
+        <RecordJoinDateInput />
+        <RecordJobInput />
+        <RecordEmailAgreementCheckbox />
+      </Form>
+    </SicilianProvider>
+  );
+}
 
-      <div>
-        <Label className="flex h-40 items-center" htmlFor="주소">
-          주소
-        </Label>
-        <Input {...register({ name: '주소' })} placeholder="주소" />
-      </div>
+function RecordNameInput() {
+  const { register } = useSicilianContext();
 
-      <div>
-        <Label className="flex h-40 items-center" htmlFor="메모">
-          메모
-        </Label>
-        <Input.TextArea {...register({ name: '메모' })} placeholder="메모" />
-      </div>
+  return (
+    <div>
+      <Label className="flex h-40 items-center" htmlFor="이름" required>
+        이름
+      </Label>
 
-      <div>
-        <Label className="flex h-40 items-center" htmlFor="가입일" required>
-          가입일
-        </Label>
-        <DatePicker
-          showToday={false}
-          getPopupContainer={handlePopupContainer}
-          onChange={(value) =>
-            selectCreateAt.onChange({
-              target: { name: '가입일', value: value.toDate() as unknown as string },
-            })
-          }
-          placeholder="가입일"
-        />
-      </div>
+      <Input {...register({ name: '이름' })} placeholder="이름" />
+    </div>
+  );
+}
 
-      <div>
-        <Label className="flex h-40 items-center" htmlFor="직업">
-          직업
-        </Label>
-        <Select
-          style={{ width: 85 }}
-          popupMatchSelectWidth={198}
-          getPopupContainer={handlePopupContainer}
-          onChange={(value) => selectJob.onChange({ target: { name: '직업', value } })}
-          options={SELECT_JOB_OPTIONS.map((option) => ({
-            value: option,
-            label: option,
-          }))}
-          defaultValue={SELECT_JOB_OPTIONS[0]}
-        />
-      </div>
+function RecordAddressInput() {
+  const { register } = useSicilianContext();
 
-      <div>
-        <Label className="flex h-40 items-center" htmlFor="이메일 수신 동의">
-          이메일 수신 동의
-        </Label>
+  return (
+    <div>
+      <Label className="flex h-40 items-center" htmlFor="주소">
+        주소
+      </Label>
 
-        <Checkbox
-          id="이메일 수신 동의"
-          name="이메일 수신 동의"
-          onChange={(e) => checkboxEmail.onChange(e as SicilianEvent)}
-        />
-      </div>
-    </Form>
+      <Input {...register({ name: '주소' })} placeholder="주소" />
+    </div>
+  );
+}
+
+function RecordMemoInput() {
+  const { register } = useSicilianContext();
+
+  return (
+    <div>
+      <Label className="flex h-40 items-center" htmlFor="메모">
+        메모
+      </Label>
+
+      <Input.TextArea {...register({ name: '메모' })} placeholder="메모" />
+    </div>
+  );
+}
+
+function RecordJoinDateInput() {
+  const { register } = useSicilianContext();
+  const selectJoinDate = register({ name: '가입일', type: 'date' });
+
+  return (
+    <div>
+      <Label className="flex h-40 items-center" htmlFor="가입일" required>
+        가입일
+      </Label>
+
+      <DatePicker
+        showToday={false}
+        getPopupContainer={(trigger) => trigger}
+        value={selectJoinDate.value ? dayjs(selectJoinDate.value) : null}
+        onChange={(value) =>
+          selectJoinDate.onChange({
+            target: { name: '가입일', value: value.toDate() as unknown as string },
+          })
+        }
+        placeholder="가입일"
+      />
+    </div>
+  );
+}
+
+function RecordJobInput() {
+  const { register } = useSicilianContext();
+  const selectJob = register({ name: '직업' });
+
+  return (
+    <div>
+      <Label className="flex h-40 items-center" htmlFor="직업">
+        직업
+      </Label>
+
+      <Select
+        style={{ width: 85 }}
+        popupMatchSelectWidth={198}
+        getPopupContainer={(trigger) => trigger}
+        onChange={(value) => selectJob.onChange({ target: { name: '직업', value } })}
+        options={SELECT_JOB_OPTIONS.map((option) => ({
+          value: option,
+          label: option,
+        }))}
+        defaultValue={SELECT_JOB_OPTIONS[0]}
+      />
+    </div>
+  );
+}
+
+function RecordEmailAgreementCheckbox() {
+  const { register } = useSicilianContext();
+  const checkboxEmail = register({ name: '이메일 수신 동의', type: 'checkbox' });
+
+  return (
+    <div>
+      <Label className="flex h-40 items-center" htmlFor="이메일 수신 동의">
+        이메일 수신 동의
+      </Label>
+
+      <Checkbox
+        id="이메일 수신 동의"
+        name="이메일 수신 동의"
+        checked={checkboxEmail.checked as unknown as boolean}
+        onChange={(e: any) => checkboxEmail.onChange(e)}
+      />
+    </div>
   );
 }
