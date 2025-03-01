ee36965e2b8d7bc97446aeef9aa2776297b258fe
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-03-02 02:20:18 +0900

    refactor: Dialog 컴포넌트 리팩토링 및 DialogContext 추가

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,18 +1,15 @@
 import { MdClose } from '@react-icons/all-files/md/MdClose';
 import { Button } from 'antd';
-import { createContext, createRef, MouseEvent, ReactNode, useContext, useEffect } from 'react';
+import { createRef, MouseEvent, ReactNode, useContext, useEffect } from 'react';
 import { DialogStore } from './DialogStore';
-
-const DialogContext = createContext<() => void>(() => {});
+import { DialogContext } from '../../hooks/Contexts/useDialogContext';
 
 export default function Dialog({}: {}) {
   const closeDialog = () => (DialogStore.store = undefined);
-
+  const dialogRef = createRef<HTMLDialogElement>();
   const handleLightDismiss = (e: MouseEvent) => {
     if ((e.target as HTMLDialogElement).nodeName === 'DIALOG') closeDialog();
   };
-
-  const dialogRef = createRef<HTMLDialogElement>();
 
   useEffect(() => {
     const dialog = dialogRef.current;
@@ -20,19 +17,16 @@ export default function Dialog({}: {}) {
     if (!dialog) return;
 
     dialog.showModal();
-
-    return () => {
-      dialog.close();
-    };
+    return () => dialog.close();
   }, [dialogRef]);
 
   return (
     <dialog
       ref={dialogRef}
-      className="backdrop:bg-[rgba(33, 33, 33, 0.4);] rounded- fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform"
+      className="backdrop:bg-[rgba(33, 33, 33, 0.4)] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform rounded-[8px]"
       onClick={handleLightDismiss}
     >
-      <DialogContext.Provider value={closeDialog}>
+      <DialogContext.Provider value={{ closeDialog }}>
         <>{DialogStore.store}</>
       </DialogContext.Provider>
     </dialog>
@@ -40,7 +34,7 @@ export default function Dialog({}: {}) {
 }
 
 Dialog.Title = ({ title }: { title: string }) => {
-  const closeDialog = useContext(DialogContext);
+  const { closeDialog } = useContext(DialogContext);
 
   return (
     <div className="flex items-center justify-between border-b-1 border-b-gray-100 px-16 py-12">
@@ -59,16 +53,17 @@ Dialog.Footer = ({
 }: {
   children: ReactNode;
   disabled?: boolean;
-  onClick: () => void;
+  onClick: (e: MouseEvent) => void;
 }) => {
-  const closeDialog = useContext(DialogContext);
+  const { closeDialog } = useContext(DialogContext);
 
   return (
     <div className="flex flex-row-reverse gap-8 border-t-1 border-t-black/6 bg-black/6 px-16 py-12">
       <Button
         type="primary"
-        onClick={() => {
-          onClick();
+        onClick={(e) => {
+          onClick(e);
+
           closeDialog();
         }}
         disabled={disabled}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './styles/tailwind.css'
+import './styles/tailwind.css';
+import { DialogWrapper } from './components/Dialog/DialogWrapper.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <DialogWrapper>
+      <App />
+    </DialogWrapper>
   </StrictMode>,
 );
