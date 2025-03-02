06ff71767bc3df11f357066002821ee49e41d2d5
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-03-02 17:06:22 +0900

    chord : react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,12 @@
-import { useRecordListStore } from './hooks/globalState/RecordList';
-import { getColumnByRecords } from './constants/getColumnByRecords';
-import { FaPlus } from '@react-icons/all-files/fa/FaPlus';
-import { Button } from 'antd';
-import { DialogStore } from './components/Dialog/DialogStore';
-import Dialog from './components/Dialog/Dialog';
-import RecordForm from './components/Form/RecordForm';
-import Table from './components/Table';
-import { useState } from 'react';
-import { Record } from './types/Record';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Landing from './pages';
 
 export default function App() {
-  const [data] = useRecordListStore();
-
   return (
-    <>
-      <Table
-        title={
-          <div className="flex justify-between align-middle">
-            <span className="flex items-center text-[16px]/[22px] font-semibold">회원 목록</span>
-            <Button type="primary" onClick={() => (DialogStore.store = RFC)}>
-              <FaPlus />
-              추가
-            </Button>
-          </div>
-        }
-        data={data}
-        columns={getColumnByRecords(data)}
-      />
-    </>
-  );
-}
-
-function RFC() {
-  const [_, dispatcher] = useRecordListStore();
-  const [formValues, setFormValues] = useState<Record>();
-
-  return (
-    <>
-      <Dialog.Title title="레코드 추가" />
-      <RecordForm setFormValues={setFormValues} />
-      <Dialog.Footer
-        onClick={() => {
-          dispatcher({ type: 'add', payload: formValues });
-        }}
-      >
-        저장
-      </Dialog.Footer>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import RecordTable from '../widgets/RecordTable';
+
+export default function Landing() {
+  return <RecordTable />;
+}
